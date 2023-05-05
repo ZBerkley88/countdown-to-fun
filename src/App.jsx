8aa2ca45ee5848417bcccd37cb2819d52a3f5021
Zack <zachary.berkley@gmail.com>
2023-05-05 08:11:56 -0400

    can add up to six large and small numbers to the game

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import React from "react";
 import Header from "./components/header/Header";
 import Timer from "./components/timer/Timer";
 import Operations from "./components/operations/Operations";
+import Numbers from "./components/numbers/Numbers";
 
 function App() {
   return (
     <>
       <Header />
       <Timer />
+      <Numbers />
       <Operations />
     </>
   );
--- src/components/operations/Operations.jsx
+++ src/components/operations/Operations.jsx
@@ -3,18 +3,35 @@ import { FaPlus, FaMinus, FaTimes, FaDivide } from "react-icons/fa";
 import "./operations.css";
 
 const Operations = () => {
+    const [result, setResult] = useState("");
+
+    function handleClick(value) {
+      setResult(result + value);
+    }
+
+    function handleClear() {
+      setResult("");
+    }
+
+    function handleEqual() {
+      try {
+        setResult(eval(result).toString());
+      } catch (error) {
+        setResult("Error");
+      }
+  }
   return (
     <div className="button_container">
-      <button className="btn">
+      <button className="operation_btn" onClick={() => handleClick("+")}>
         <FaPlus className="btn_icon" />
       </button>
-      <button className="btn">
+      <button className="operation_btn" onClick={() => handleClick("-")}>
         <FaMinus />
       </button>
-      <button className="btn">
+      <button className="operation_btn" onClick={() => handleClick("*")}>
         <FaTimes />
       </button>
-      <button className="btn">
+      <button className="operation_btn" onClick={() => handleClick("/")}>
         <FaDivide />
       </button>
     </div>
